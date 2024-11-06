31f373330bca5c0089f46555abceeb9403c7550f
jordanjust34 <jordanjust34@gmail.com>
2024-11-06 10:54:38 -0600

    Deleted comment block that had nothing to do with it.

--- code.c
+++ code.c
@@ -1,31 +1,3 @@
-/*
-    As we've been spending a couple weeks on file I/O, I'd like you to implement a system for
-saving, reading, and editing data. You'll need to create some kind of structure to store
-some kind of information. Players from a sports team, Pokémon, foods, or some other
-thing that would be stored in a structure. Your program should be menu based, where
-running the program presents the user with a list of options. A user should be able to:
-
-    1. Insert a new file entry at the end of the file
-    2. Read an entry from the file at an index
-    3. Edit an existing entry at an index
-    4. Exit the Program
-
-The name of the file should be supplied as a command line argument and passed to
-relevant fopen calls. Your program should use binary files and methods for file I/O, not
-text methods. This program will not use CodeBox, so you have some more creative
-flexibility than most assignments.
-
-To submit, I'd like you to have a folder in your home directory called fileio-hw . Inside of
-that directory should be your completed code file, called main.c , a compiled binary called
-out (can be compiled as gcc main.c -o out ), and a sample data file called data.bin .
-These names are only required once you're done with the assignment, so feel free to use
-other file names and just a.out while you're working on it. In D2L, submit screenshots of
-your code working.
-*/
-
-// 'rb+' will allow us to read and write to files
-// Write the files in binary
-
 #include <stdio.h>
 // For the "system("cls");"
 #include <stdlib.h>
